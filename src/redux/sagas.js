86212ff5f399b3113b55c521ea5d17d071579cf8
Sumudu Herath <sumudu.herath@circles.asia>
2021-09-16 09:39:56 +0530

    code cleaned

--- src/redux/sagas.js
+++ src/redux/sagas.js
@@ -6,27 +6,27 @@ import {
   EDIT_NOTE_ACTION_SUCCESS,
   DELETE_NOTE_ACTION,
   DELETE_NOTE_ACTION_SUCCESS,
-  CONFIG_RESPONSE_ACTION, CONFIG_SETTINGS_ACTION,ERROR_ACTION
+  CONFIG_RESPONSE_ACTION, CONFIG_SETTINGS_ACTION, ERROR_ACTION
 } from "./actions/actionTypes";
 import {
   createNote,
-  editNote,
   updateNote,
   deleteNote,
   configFetchData,
 } from "../fetchApi";
 
 export function* cddSaga({ payload }) {
-    try {
-      const configData = yield call(configFetchData, payload.Main_Header);
-      if(configData.login_Header === ""){
-        console.log("LOGIN HEADER IS NULL");
-        yield put({ type: ERROR_ACTION, payload: configData });
-      }else{
-      yield put({ type: CONFIG_RESPONSE_ACTION, payload: configData });}
-    }catch (e) {
+  try {
+    const configData = yield call(configFetchData, payload.Main_Header);
+    if (configData.login_Header === "") {
+      console.log("LOGIN HEADER IS NULL");
+      yield put({ type: ERROR_ACTION, payload: configData });
+    } else {
+      yield put({ type: CONFIG_RESPONSE_ACTION, payload: configData });
     }
+  } catch (e) {
   }
+}
 
 export function* addNoteSaga({ payload }) {
   try {
@@ -38,7 +38,7 @@ export function* addNoteSaga({ payload }) {
     );
     yield put({ type: ADD_NOTE_ACTION_SUCCESS, payload: note });
   } catch (e) {
-    //console.log("Error occurred ", e.message);
+
   }
 }
 
@@ -61,12 +61,12 @@ export function* deleteNoteSaga({ payload }) {
     const note = yield call(deleteNote, payload.noteId);
     yield put({ type: DELETE_NOTE_ACTION_SUCCESS, payload: note });
   } catch (e) {
-    //console.log("Error occurred ", e.message);
+
   }
 }
 
 export function* mySaga() {
-    yield takeLatest(CONFIG_SETTINGS_ACTION, cddSaga);
+  yield takeLatest(CONFIG_SETTINGS_ACTION, cddSaga);
   yield takeLatest(ADD_NOTE_ACTION, addNoteSaga);
   yield takeLatest(EDIT_NOTE_ACTION, editNoteSaga);
   yield takeLatest(DELETE_NOTE_ACTION, deleteNoteSaga);
